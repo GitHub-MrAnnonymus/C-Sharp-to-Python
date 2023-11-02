d1ee08152b15df28c4d1d49096a364ea8ce437da
GitHub-MrAnnonymus <120578546+GitHub-MrAnnonymus@users.noreply.github.com>
2023-11-02 20:56:52 +0100

    Add files via upload

---
+++ src/CSharpToPython.Tests/ClassTests.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using Xunit;
+
+namespace CSharpToPython.Tests {
+    public class ClassTests {
+
+        private readonly EngineWrapper engine = new EngineWrapper();
+
+        [Fact]
+        public void SimpleClassWorks() {
+            var code = @"
+public class SomeClass {
+    int Main() { return 1; }
+}";
+            var rslt = Program.ConvertAndRunCode(engine, code);
+            Assert.NotNull(rslt);
+        }
+
+        [Fact]
+        public void SimpleStructWorks() {
+            var code = @"
+public struct SomeStruct {
+    int Main() { return 1; }
+}";
+            var rslt = Program.ConvertAndRunCode(engine, code);
+            Assert.NotNull(rslt);
+        }
+
+        [Fact]
+        public void ClassBaseTypesWork() {
+            var code = @"
+public class SomeBaseClass { public int GetInt() => 1; }
+public class SomeClass : SomeBaseClass {
+}";
+            var rslt = Program.ConvertAndRunCode(engine, code);
+            Assert.Equal(1, (object)((dynamic)rslt).GetInt());
+        }
+
+        [Fact]
+        public void ClassConstructorCanCallBaseClassConstructor() {
+            var code = @"
+public class BaseClass : object {
+   public BaseClass(int x) { this.TheInt = x; }
+   int TheInt ;
+   int GetInt() => this.TheInt;
+}
+public class SomeClass : BaseClass {
+    public SomeClass() : base(1) { }
+}";
+            var rslt = Program.ConvertAndRunCode(engine, code);
+            Assert.Equal(1, (object)((dynamic)rslt).GetInt());
+        }
+        [Fact]
+        public void ClassConstructorCallsBaseClassConstructor() {
+            var code = @"
+public class BaseClass : object {
+   public BaseClass() { this.TheInt = 1; }
+   int TheInt ;
+   int GetInt() => this.TheInt;
+}
+public class SomeClass : BaseClass {
+    public SomeClass() { }
+}";
+            var rslt = Program.ConvertAndRunCode(engine, code);
+            Assert.Equal(1, (object)((dynamic)rslt).GetInt());
+        }
+
+        [Fact]
+        public void InstanceMethodWorks() {
+            var code = @"
+public class SomeClass {
+    public int GetInt() { return 1; }
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int theInt = rslt.GetInt();
+            Assert.Equal(1, theInt);
+        }
+
+        [Fact]
+        public void OverrideMethodCanCallBase() {
+            var code = @"
+public class SomeBaseClass {
+    public virtual int GetInt() => 1;
+}
+public class SomeClass : SomeBaseClass {
+    public override int GetInt() => base.GetInt();
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int theInt = rslt.GetInt();
+            Assert.Equal(1, theInt);
+        }
+
+        [Fact]
+        public void StaticMethodWorks() {
+            var code = @"
+public class SomeClass {
+    public static int StaticGetInt() { return 1; }
+    public int GetInt() { return SomeClass.StaticGetInt(); }
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int theInt = rslt.GetInt();
+            Assert.Equal(1, theInt);
+        }
+
+        /// <summary>
+        /// Similar to <see cref="FunctionTests.FunctionWithParameterWorks"/> except that test passes no arguments
+        /// </summary>
+        [Fact]
+        public void ParamsMethodParameterWorks() {
+            var code = @"
+public class SomeClass {
+    public int[] GetInts(params int[] intVals) { return intVals; }
+    public int GetIntArray() { return this.GetInts(1, 2); }
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            System.Collections.Generic.IEnumerable<object> theInt = rslt.GetIntArray();
+            Assert.Equal(new object[] { 1, 2 }, theInt);
+        }
+
+        [Fact]
+        public void FieldWithInitializerWorks() {
+            var code = @"
+public class SomeClass {
+    public int SomeInt = 1;
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int someInt = rslt.SomeInt;
+            Assert.Equal(1, someInt);
+        }
+        [Fact]
+        public void FieldWithoutInitializerWorks() {
+            var code = @"
+public class SomeClass {
+    public object SomeValue;
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            object someValue = rslt.SomeValue;
+            Assert.Null(someValue);
+        }
+        [Fact]
+        public void FieldWithMultipleVariablesWorks() {
+            var code = @"
+public class SomeClass {
+    public object SomeValue, SomeValue2;
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            object someValue = rslt.SomeValue;
+            Assert.Null(someValue);
+        }
+        [Fact]
+        public void StaticFieldWithoutInitializerWorks() {
+            var code = @"
+public class SomeClass {
+    public static object SomeValue;
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            object someValue = rslt.SomeValue;
+            Assert.Null(someValue);
+        }
+        [Fact(Skip = "Not implemented yet. Need to figure out best way to get default value for a type.")]
+        public void ValueTypeFieldWithoutInitializerWorks() {
+            var code = @"
+public class SomeClass {
+    public int SomeValue;
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            object someValue = rslt.SomeValue;
+            Assert.Equal(0, someValue);
+        }
+
+        [Fact]
+        public void PropertyWorks() {
+            var code = @"
+public class SomeClass {
+    int _someValue;
+    public int SomeValue { get { return this._someValue; } set { this._someValue = value; }
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            rslt.SomeValue = 2;
+            int someValue = rslt.SomeValue;
+            Assert.Equal(2, someValue);
+        }
+
+        [Fact]
+        public void ExpressionBodiedPropertyWorks() {
+            var code = @"
+public class SomeClass {
+    public int SomeValue => 1;
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int someValue = rslt.SomeValue;
+            Assert.Equal(1, someValue);
+        }
+
+        [Fact]
+        public void AutoPropertyWorks() {
+            var code = @"
+public class SomeClass {
+    public int SomeValue { get; set; } = 1;
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int someValue = rslt.SomeValue;
+            Assert.Equal(1, someValue);
+        }
+
+        [Fact(Skip = "Not sure how to implement static properties")]
+        public void StaticPropertyWorks() {
+            var code = @"
+public class SomeClass {
+    public static int SomeValue { get { return 1; } }
+    public int GetInt() { return SomeClass.SomeValue; }
+}";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int someValue = rslt.GetInt();
+            Assert.Equal(1, someValue);
+        }
+
+        [Fact]
+        public void NamespaceWorks() {
+            var code = @"
+namespace SomeNamespace {
+    public class SomeClass { }
+}";
+            var rslt = Program.ConvertAndRunCode(engine, code);
+            Assert.NotNull(rslt);
+        }
+
+        [Fact]
+        public void UsingDirectiveWorks() {
+            var code = @"
+using System;
+class SomeClass {
+   public object GetObj() { return new Random() ; }
+}
+";
+            dynamic someClass = Program.ConvertAndRunCode(engine, code);
+            var obj = (object)someClass.GetObj();
+            Assert.IsType<System.Random>(obj);
+        }
+        [Fact]
+        public void UsingDirectiveWithAliasWorks() {
+            var code = @"
+using SOMEALIAS = System;
+class SomeClass {
+   public object GetObj() { return new SOMEALIAS.Random() ; }
+}
+";
+            dynamic someClass = Program.ConvertAndRunCode(engine, code);
+            var obj = (object)someClass.GetObj();
+            Assert.IsType<System.Random>(obj);
+        }
+        [Fact]
+        public void UsingStaticDirectiveDoesntWork() {
+            var code = @"
+using static SOMECLASS;
+";
+            var converted = Program.ConvertCode(code);
+            Assert.Contains("#ERROR", converted);
+        }
+
+        [Fact]
+        public void InterfaceIsIgnored() {
+            var code = "interface ISomeInterface {}";
+            Assert.Equal("", Program.ConvertCode(code));
+        }
+        [Fact]
+        public void NestedInterfaceIsIgnored() {
+            var code = "class Blah { interface ISomeInterface {} }";
+            Assert.NotNull(Program.ConvertAndRunCode(engine, code));
+        }
+
+        [Fact]
+        public void NestedClassesWork() {
+            var code = @"
+class SomeClass {
+    public class SomeInnerClass {
+        public int GetInt() => 1;
+    }
+}
+class SomeOtherClass {
+    public int GetSomeClassInt() => new SomeClass.SomeInnerClass().GetInt();
+}
+";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int someValue = rslt.GetSomeClassInt();
+            Assert.Equal(1, someValue);
+        }
+
+        [Fact]
+        public void EnumsWork() {
+            var code = @"
+enum SomeEnum { x, y = 3 }
+class SomeClass {
+    public object GetEnumVal() => SomeEnum.x;
+}
+";
+            dynamic rslt = Program.ConvertAndRunCode(engine, code);
+            int someValue = rslt.GetEnumVal();
+            Assert.Equal(0, someValue);
+        }
+    }
+}
---
+++ src/CSharpToPython.Tests/ExpressionTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Xunit;
+
+namespace CSharpToPython.Tests {
+    public class ExpressionTests {
+
+        private readonly EngineWrapper engine = new EngineWrapper();
+
+        [Fact]
+        public void DoubleConversionWorks() {
+            var rslt = Program.ConvertAndRunExpression(engine, "1.0");
+            Assert.Equal(1.0, rslt);
+            Assert.NotEqual(1, rslt);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("\"Hello\"", "Hello")]
+        [InlineData("\"Something \\\"in\\\" quotes\"", "Something \"in\" quotes")]
+        [InlineData("true", true)]
+        [InlineData("null", null)]
+        [InlineData("'a'", "a")]
+        [InlineData(@"'\\'", @"\")]
+        public void LiteralConversionWorks(string code, object expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunExpression(engine, code));
+        }
+
+        [Theory]
+        [InlineData("$\"One {1} two {2}\"", "One 1 two 2")]
+        [InlineData("$\"{1}\"", "1")]
+        [InlineData("$\"{{1}}\"", "{1}")]
+        public void InterpolatedStringsWork(string code, string expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunExpression(engine, code));
+        }
+
+        [Theory]
+        [InlineData("@\"a\\b\"", "a\\b")]
+        [InlineData("@\"a\"\"b\"", "a\"b")]
+        public void RawStringsWork(string code, string expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunExpression(engine, code));
+        }
+
+        [Theory]
+        [InlineData("1+1", 2)]
+        [InlineData("1-1", 0)]
+        [InlineData("true && true", true)]
+        [InlineData("true || false", true)]
+        [InlineData("1 == 1", true)]
+        [InlineData("1 != 2", true)]
+        [InlineData("1 >= 1", true)]
+        [InlineData("1 > 0", true)]
+        [InlineData("1 <= 1", true)]
+        [InlineData("1 < 2", true)]
+        [InlineData("3 % 2", 3 % 2)]
+        [InlineData("4 << 1", 4 << 1)]
+        [InlineData("4 >> 1", 4 >> 1)]
+        [InlineData("3 & 1", 3 & 1)]
+        [InlineData("2 | 1", 2 | 1)]
+        [InlineData("2 ^ 1", 2 ^ 1)]
+        [InlineData("null ?? new int[0]", new int [0])]
+        [InlineData("new int[0] ?? null", null)] // This does NOT match C#...
+        [InlineData("new [] {1} ?? null", new[] { 1 })]
+        public void BinaryOperatorsWork(string code, object expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunExpression(engine, code));
+        }
+
+        [Theory]
+        [InlineData("!false", true)]
+        [InlineData("-(1)", -1)]
+        [InlineData("~(5)", ~5)]
+        public void UnaryOperatorsWork(string code, object expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunExpression(engine, code));
+        }
+
+        [Theory]
+        [InlineData("(double)1", 1.0)]
+        [InlineData("(int)1.2", 1)]
+        [InlineData("(char)97", "a")]
+        [InlineData("(object)1", 1)]
+        public void CastsWork(string code, object expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunStatements(engine, "return " + code));
+        }
+
+        [Fact]
+        public void AsExpressionWorks() {
+            Assert.NotNull(Program.ConvertAndRunExpression(engine, "new object() as object"));
+            Assert.Null(Program.ConvertAndRunExpression(engine, "null as string"));
+        }
+
+        [Fact]
+        public void IsExpressionWorks() {
+            Assert.Equal(true, Program.ConvertAndRunExpression(engine, "1 is int"));
+        }
+        [Fact(Skip = "Not implemented yet")]
+        public void PatternMatchExpressionWorks() {
+            Assert.Equal(1, Program.ConvertAndRunExpression(engine, "1 is int i ? i : 0"));
+        }
+
+        [Fact]
+        public void TypeofExpressionWorks() {
+            Assert.True((bool)Program.ConvertAndRunExpression(engine, "typeof(object) == typeof(object)"));
+        }
+
+        [Theory]
+        [InlineData("new [] {1, 2}", new [] { 1, 2 })]
+        [InlineData("new int[] {1, 2}", new [] { 1, 2 })]
+        [InlineData("new int[0]", new int[0])]
+        public void ArraysWork(string code, object expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunExpression(engine, code));
+        }
+
+        [Fact]
+        public void ArrayIndexingWorks() {
+            Assert.Equal(1, Program.ConvertAndRunExpression(engine, "new [] {1, 2}[0]"));
+        }
+
+        [Fact(Skip = "Not implemented yet")]
+        public void DictionaryWorks() {
+            var dict = Program.ConvertAndRunStatements(engine, 
+                "return new System.Collections.Generic.Dictionary<int, int> { { 1, 1 } }",
+                new[] { "System" });
+            Assert.Equal(1, ((dynamic)dict)[1]);
+        }
+
+        [Theory]
+        [InlineData("true ? 1 : 2", 1)]
+        public void TernaryExpressionWorks(string code, object expectedResult) {
+            Assert.Equal(expectedResult, Program.ConvertAndRunExpression(engine, code));
+        }
+
+        [Fact]
+        public void SingleArgumentLambdaWorks() {
+            Assert.Equal(1, Program.ConvertAndRunExpression(engine, "(a => a)(1)"));
+        }
+
+        [Fact]
+        public void MultiArgumentLambdaWorks() {
+            Assert.Equal(2, Program.ConvertAndRunExpression(engine, "((a, b) => a + b)(1, 1)"));
+        }
+
+        [Theory]
+        [InlineData("new { a = 2 }", "AnonymousObject(a = 2)")]
+        [InlineData("new { a }", "AnonymousObject(a = a)")]
+        public void AnonymousObjectsWork(string code, string expected) {
+            var converted = Program.ConvertExpressionCode(code);
+            Assert.Equal(expected, converted);
+        }
+    }
+}
